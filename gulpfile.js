d572f31d59b8492724b6b04816faf9380da5825d
Fabricio Catae <fabricio.catae@microsoft.com>
2016-03-27 13:16:00 -0300

    gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
+var watch = require('gulp-watch');
+var debug = require('gulp-debug');
 
-gulp.task('html', function() {
-   
-   return gulp.src([
+var static_files = [
        'src/client/**/*.html',
        'src/client/**/*.css',
        'src/client/**/*.js',
        'src/client/**/*.png',
        'src/client/**/*.jpg'
-       ])
+       ];
+        
+gulp.task('html', function() {
+   
+   return gulp.src(static_files)
        .pipe(gulp.dest('dist/client/'));
     
 });
+
+watch(static_files)
+    .pipe(debug('watch'))
+    .pipe(gulp.dest('dist/client/'))
+    
