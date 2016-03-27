e950b1b75908496767cf1df43c7f4d099be2477d
Fabricio Catae <fabricio.catae@microsoft.com>
2016-03-27 17:21:11 -0300

    selecionaImagem (PNG)

--- src/client/index.js
+++ src/client/index.js
@@ -11,4 +11,19 @@ function drop(ev) {
      var data = ev.dataTransfer.getData("text");
      ev.target.appendChild(document.getElementById(data));
 }
+
+function selecionaImagem(num, tipo) {
+    var num_array = ['4', '5', '6', '7', 'queen', 'jack', 'king', 'ace', '2', '3'];
+    var tipo_array = ['diamonds', 'spades', 'hearts', 'clubs'];
+    
+    var c1 = num_array[num];
+    var c2 = '_of_';
+    var c3 = tipo_array[num];
+    var c4 = '.png';
+    
+    return c1+c2+c3+c4;
+}
  
+function selectionarCarta() {
+    
+}
