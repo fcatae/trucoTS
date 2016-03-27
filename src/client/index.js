43026ba109b5f775ee1a52b5b060f26cb274f9c9
Fabricio Catae <fabricio.catae@microsoft.com>
2016-03-27 17:45:48 -0300

    seleciona carta

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,17 @@
+$(document).ready(function() {
+
+    selectionarCarta('p1', {num: 0, tipo: 1});
+    selectionarCarta('p2', {num: 3, tipo: 2});
+    selectionarCarta('p3', {num: 8, tipo: 3});
+
+    selectionarCarta('m', {num: 7, tipo: 1});
+    
+    selectionarCarta('c1', {num: -1, tipo: -1});
+    selectionarCarta('c2', {num: -1, tipo: -1});
+    selectionarCarta('c3', {num: -1, tipo: -1});
+    
+});
+  
 function allowDrop(ev) {
      ev.preventDefault();
  }
@@ -16,14 +30,31 @@ function selecionaImagem(num, tipo) {
     var num_array = ['4', '5', '6', '7', 'queen', 'jack', 'king', 'ace', '2', '3'];
     var tipo_array = ['diamonds', 'spades', 'hearts', 'clubs'];
     
+    if( num<0 || tipo<0 ) {
+        return "images/backside.png";
+    }
+    
+    var c0 = 'images/';
     var c1 = num_array[num];
     var c2 = '_of_';
-    var c3 = tipo_array[num];
+    var c3 = tipo_array[tipo];
     var c4 = '.png';
     
-    return c1+c2+c3+c4;
+    return c0+c1+c2+c3+c4;
 }
- 
-function selectionarCarta() {
+
+function selectionarCarta(name, carta) {
     
+    var elem = document.getElementById(name);
+    var imagem;
+        
+    if(carta != null) {
+        var num = carta.num;
+        var tipo = carta.tipo;
+        imagem = selecionaImagem(num, tipo);
+    }
+    else {
+        imagem = "";
+    }
+    elem.src = imagem;
 }
